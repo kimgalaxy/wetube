c720d6c9f149eef8e68a94769b2745ab9ef8b9bb
kimgalaxy <55799302+kimgalaxy@users.noreply.github.com>
2020-01-25 05:57:13 +0900

    #2.4 Handling Routes with Express

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ function handleLitening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  res.send("Hello from home!");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleLitening);
