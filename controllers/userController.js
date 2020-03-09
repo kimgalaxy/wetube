bb7b424b69c068b3875dd03bff110ace21820aaf
kimgalaxy <55799302+kimgalaxy@users.noreply.github.com>
2020-03-10 02:38:35 +0900

    #2.24 LogIn & User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,7 +20,12 @@ export const postJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Log Out" });
 export const userDetail = (req, res) =>
