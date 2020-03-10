b5a0de1ec22cbe8881452b3f54bdc075763cfa6f
kimgalaxy <55799302+kimgalaxy@users.noreply.github.com>
2020-03-10 19:59:08 +0900

    #3.4 Comment Model

--- models/Video.js
+++ models/Video.js
@@ -17,7 +17,13 @@ const VideoSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video"
+    }
+  ]
 });
 
 const model = mongoose.model("Video", VideoSchema);
